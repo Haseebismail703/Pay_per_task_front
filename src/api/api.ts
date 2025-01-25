fcc9a8b151fcea388a63c4f93390ee6c1e3fbe10
haseebismail703 <haseebismail703@gmail.com>
2025-01-25 21:47:54 +0500

    Complete100%

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,3 +1,3 @@
 // let api: string = 'http://localhost:5000/api'
-let api : string = 'https://ppt-backend-gqijrooxf-haseebismail703s-projects.vercel.app/api'
+let api : string = 'https://ppt-backend-black.vercel.app/api'
 export default api 
